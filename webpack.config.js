9ccd69bb7dd49c67a2e07b2782f7f1e0d8bf4cea
Amaan Lakhani <amaan.lakhani5@gmail.com>
2022-08-10 17:32:26 -0400

    updating firebase to attempt to incorporate react

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+  // The entry point file described above
+  entry: './client/index.js',
+  // The location of the build folder described above
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  // Optional and for development only. This provides the ability to
+  // map the built code back to the original source format when debugging.
+  devtool: 'eval-source-map',
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            '@babel/preset-react'
+          ]
+        }
+      }
+    ]
+  }
+};
